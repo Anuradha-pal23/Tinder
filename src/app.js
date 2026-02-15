7e55fe04f8ce92fa98151949fe113223cd7cf04d
Anuradha Pal <anuradhakp2004@gmail.com>
2026-02-15 18:54:41 +0530

    Api level data  sanitazation

--- src/app.js
+++ src/app.js
@@ -4,25 +4,70 @@ const app=express();
 const connectDb=require('./config/ConnectDb');
 const User=require("./models/users")
 
-app.post("/signup",async(req,res)=>{
-const user=new User({
-    firstName:"Anuradha",
-    lastName:"pal",
-    emailId:"anu@gmail.com",
-    password:"anu123",
-    
-})
+app.use(express.json()) 
 
-try {
-     await user.save();
- res.send("user created successfully");
-}
- catch (error) {
-    res.status(400).send("something wrong");
-    console.log("something went wrong.....");
-}
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const savedUser = await user.save();
+
+    res.status(201).json({
+      message: "user created successfully",
+      data: savedUser,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send("something went wrong");
+  }
 });
 
+
+
+//get user by email
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+
+    try {
+        console.log(userEmail);
+        const user=await User.findOne({emailId:userEmail});
+        if(!user){
+            res.status(404).send("user not found")
+        }
+        else{
+            res.send(user)
+        }
+    } catch (error) {
+          res.status(400).send("something wrong");
+    }
+})
+
+//delete by id
+app.delete("/user/:id",async(req,res)=>{
+const userId=req.params.id;
+try {
+    const user=await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+} catch (error) {
+    res.status(404).send("something went wrong");
+}
+})
+
+//update data by user
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try {
+        const user=await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument:"after",
+            runValidators:true,
+        });
+        console.log(user);
+        res.send("user updated successfully");
+}
+catch(error){
+    res.status(400).send("something went wrong");
+}
+});
 
 connectDb().
 then(()=>{
--- src/models/users.js
+++ src/models/users.js
@@ -1,24 +1,59 @@
 const mongoose=require('mongoose');
 
 const userSchema=new mongoose.Schema({
+    userId:{
+        type:String,      
+        //   required:true,
+    },
     firstName:{
-        type:String
+        type:String,
+        required:true,
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        validate(value){
+            const emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            if(!emailRegex.test(value)){
+                throw new Error("email is not valid");
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        validate(value){
+            if(value.length<6){
+                throw new Error("password must be atleast 6 character");
+            }
+        }
     },
     age:{
-        type:Number
+        type:Number,
+        required:true,
+        validate(value){
+            if(value<18){
+                throw new Error("age must be atleast 18");
+                
+            }
+        }
     },
     gender:{
-        type:String
-    },
+        type:String,
+        required:true,
+        validate(value){
+            if(!["male","female","other"].includes(value.toLowerCase())){
+                throw new Error("gender is not valid");
+        }
+    }
+}
+},
+{
+    timestamps:true,
     
 })
 const users=mongoose.model("User",userSchema);
